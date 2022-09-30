7e98cdba106e101db613fa5a251ed335ba909e87
Evgeniy Kozevnikov <104453884+EvegnBeluga@users.noreply.github.com>
2022-09-30 19:07:36 +0500

    изменения

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,9 +2,18 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    User user = new User();
+    Connection connection = getConnection();
+
+    private Connection getConnection() {
+        return connection;
+    }
+
     public UserDaoJDBCImpl() {
 
     }
@@ -17,16 +26,53 @@ public class UserDaoJDBCImpl implements UserDao {
 
     }
 
-    public void saveUser(String name, String lastName, byte age) {
+    public void saveUser(String name, String lastName, byte age) throws SQLException {
+        Statement prepareStatement;
+        String sql = "INSERT INTO MYSQL(NAME, LASTNAME, AGE) value (?, ?, ?)";
 
+        prepareStatement = connection.prepareStatement(sql);
+       // prepareStatement.setCursorName(name);
+//       prepareStatement.set
+
+        // prepareStatement.e
+        prepareStatement.close();
+        if(connection != null) {
+            connection.close();
+        }
     }
 
-    public void removeUserById(long id) {
-
+    public void removeUserById(long id) throws SQLException {
+        PreparedStatement preparedStatement;
+        String sql = "DELETE FROM USER WHERE ID=?";
+        preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setLong(1,user.getId());
+        preparedStatement.executeUpdate();
+        preparedStatement.close();
+        if(connection != null) {
+            connection.close();
+        }
     }
 
-    public List<User> getAllUsers() {
-        return null;
+    public List<User> getAllUsers() throws SQLException {
+        List<User> userList = new ArrayList<>();
+        String sql = "SELECT ID, NAME, LASTNAME, AGE";
+        Statement statement;
+        statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next()) {
+
+            user.setId(resultSet.getLong("ID"));
+            user.setName(resultSet.getString("NAME"));
+            user.setLastName(resultSet.getString("LASTNAME"));
+            user.setAge(resultSet.getByte("AGE"));
+            userList.add(user);
+        }
+        statement.close();
+        if(connection != null) {
+            connection.close();
+        }
+        return userList;
     }
 
     public void cleanUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static void main(String[] args) {
+         String URL = "jdbc:mysql://localhost:3306/mysql";
+         String USERNAME = "root";
+         String PASSWORD = "9949";
+        Connection connection;
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            // System.out.println("соединение установлено");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        // реализуйте настройку соеденения с БД
+    }
+
+    public static void getConnection() {
+    }
 }
